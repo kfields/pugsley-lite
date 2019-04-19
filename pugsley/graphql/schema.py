7296d9506d3eb57c29f3e8f16b81ab857f3fd3b2
Kurtis Fields <kurtisfields@gmail.com>
2019-04-19 13:18:33 -0400

    more graphql

--- pugsley/graphql/schema.py
+++ pugsley/graphql/schema.py
@@ -1,6 +1,7 @@
 import graphene
 from graphene import relay
 from graphene_sqlalchemy import SQLAlchemyObjectType, SQLAlchemyConnectionField
+from pugsley import db
 from pugsley.models import User, Post
 
 
@@ -25,12 +26,53 @@ class PostConnection(relay.Connection):
     class Meta:
         node = PostNode
 
+class UpdatePostInput(graphene.InputObjectType):
+    id = graphene.ID(required=True)
+    title = graphene.String()
+    body = graphene.String()
+'''
+class UpdatePost(graphene.Mutation):
+    class Arguments:
+        post_data = UpdatePostInput(required=True)
+
+    post = graphene.Field(PostNode)
+
+    @staticmethod
+    def mutate(root, info, post_data=None):
+        post = Post.query.filter_by(id=post_data.id).first()
+        post.title = post_data.title
+        post.body = post_data.body
+        post.save()
+        return UpdatePost(post=post)
+'''
+class UpdatePost(graphene.Mutation):
+    class Arguments:
+        id = graphene.ID(required=True)
+        title = graphene.String()
+        body = graphene.String()
+
+    ok = graphene.Boolean()
+
+    def mutate(self, info, id, title, body):
+        print('mutate')
+        print(id)
+        # post = Post.query.get(id)
+        post = graphene.Node.get_node_from_global_id(info, id)
+        print(post)
+        post.title = title
+        post.body = body
+        db.session.commit()
+        ok = True
+
+        return ok
+
+class MyMutations(graphene.ObjectType):
+    update_post = UpdatePost.Field()
 
 class Query(graphene.ObjectType):
     node = relay.Node.Field()
-    # Allows sorting over multiple columns, by default over the primary key
+    post = relay.Node.Field(PostNode)
     all_users = SQLAlchemyConnectionField(UserConnection)
-    # Disable sorting over this field
-    all_posts = SQLAlchemyConnectionField(PostConnection, sort=None)
+    all_posts = SQLAlchemyConnectionField(PostConnection)
 
-schema = graphene.Schema(query=Query)
+schema = graphene.Schema(query=Query, mutation=MyMutations)

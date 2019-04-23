2eb77bac1e404f34cdd5eed7bfe9386e698e6792
Kurtis Fields <kurtisfields@gmail.com>
2019-04-23 14:14:41 -0400

    nuke internal mc

--- pugsley/mc/__init__.py
+++
@@ -1,7 +0,0 @@
-from flask import Blueprint
-from flask_cors import CORS
-
-bp = Blueprint('mc', __name__)
-CORS(bp)
-
-from pugsley.mc import routes
--- pugsley/mc/routes.py
+++
@@ -1,6 +0,0 @@
-from flask import render_template, request, current_app, redirect
-from pugsley.mc import bp
-
-@bp.route('/mc/')
-def mc():
-    return redirect("/static/mc/index.html")
